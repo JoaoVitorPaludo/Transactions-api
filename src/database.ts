45e413e36123ef8b113b2eabd4f7b0a63078c886
JoaoVitorPaludo <joaovpaludo@gmail.com>
2025-03-07 10:37:21 -0300

    feat: Configurando variáveis de ambiente e validação com zod

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,15 @@
+import "dotenv/config";
 import { Knex, knex as setupKnex } from "knex";
+import { env } from "./env";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL must be set");
+}
 
 export const config: Knex.Config = {
   client: "sqlite",
   connection: {
-    filename: "./db/app.db",
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,23 @@
 import fastify from "fastify";
+import crypto from "node:crypto";
 import { knex } from "./database";
-
+import { env } from "./env";
 const app = fastify();
 
 app.get("/", async () => {
-  const test = await knex("sqlite_schema").select("*");
-  return test;
+  const transaction = await knex("transactions")
+    .insert({
+      id: crypto.randomUUID(),
+      title: "Transação de teste",
+      amount: 100,
+    })
+    .returning("*");
+  return transaction;
 });
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log("Server is running on port 3333");
