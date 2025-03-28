49ae3b83f103a477eb5647145bc313517b60583f
JoaoVitorPaludo <joaovpaludo@gmail.com>
2025-03-28 10:46:15 -0300

    feat: Implementando fluxo de transações e id de sessão

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,12 @@
+import cookie from "@fastify/cookie";
 import fastify from "fastify";
-import crypto from "node:crypto";
-import { knex } from "./database";
 import { env } from "./env";
+import { transactionsRoutes } from "./routes/transactions";
 const app = fastify();
 
-app.get("/", async () => {
-  const transaction = await knex("transactions")
-    .insert({
-      id: crypto.randomUUID(),
-      title: "Transação de teste",
-      amount: 100,
-    })
-    .returning("*");
-  return transaction;
+app.register(cookie);
+app.register(transactionsRoutes, {
+  prefix: "transactions",
 });
 
 app
